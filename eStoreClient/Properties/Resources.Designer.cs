198f171f69009f2cea582d5c1516b8f56f4c5748
buiquydangsnd1@gmail.com <buiquydangsnd1@gmail.com>
2023-07-25 09:05:38 +0700

    update font-end

--- eStoreClient/Properties/Resources.Designer.cs
+++ eStoreClient/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace eStoreClient.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap check_mark {
+            get {
+                object obj = ResourceManager.GetObject("check-mark", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap delete_button {
             get {
                 object obj = ResourceManager.GetObject("delete-button", resourceCulture);
@@ -106,6 +116,16 @@ namespace eStoreClient.Properties {
         internal static System.Drawing.Bitmap delete_button1 {
             get {
                 object obj = ResourceManager.GetObject("delete-button1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap delete_button2 {
+            get {
+                object obj = ResourceManager.GetObject("delete-button2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
